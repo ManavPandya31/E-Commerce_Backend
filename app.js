c1e62e7dfbc5b5f39818d83019c797e5de19d553
ManavPandya31 <manavpandya42@gmail.com>
2026-02-26 11:09:12 +0530

    update in cors

--- app.js
+++ app.js
@@ -11,7 +11,16 @@ import compression from "compression";
 const app = express();
 
 app.use(compression());
-app.use(cors());
+//app.use(cors());
+// app.use(cors({
+//   origin: "http://localhost:5173",
+//   credentials: true
+// }));
+app.use(cors({
+  origin: "https://shoppzo.netlify.app",
+  credentials: true
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
